b88a09b9a4ea4b871d602abda2c6993c1ab8020e
bielakarolina <bielakarolina77@gmail.com>
2018-11-24 17:43:31 +0100

    Start

--- src/Model/Main.java
+++ src/Model/Main.java
@@ -1,4 +1,31 @@
 package Model;
 
-public class Main {
-}
+import javafx.application.Application;
+import javafx.scene.control.Menu;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.io.InputStream;
+
+public class Main extends Application {
+
+    String fontName = "stylesheets/ARCADECLASSIC.TTF";
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+
+        InputStream fontStream = Main.class.getResourceAsStream(fontName);
+        if (fontStream != null) {
+            Font gameFont = Font.loadFont(fontStream, 36);
+            fontStream.close();
+
+            Menu menu = new Menu();
+            menu.showMenu();
+        }
+
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
